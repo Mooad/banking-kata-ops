7477c4f871276c6bcf6d8d0984946b578303599f
Fajri <fajri@fr.jcdecaux.org>
2022-06-25 12:30:07 +0200

    [MFA] : [Banking-Kata] : Initialise Repo and add tests

---
+++ src/test/java/kata/domain/AccountOperationTest.java
@@ -0,0 +1,47 @@
+package kata.domain;
+
+import org.junit.Test;
+
+
+
+public class AccountOperationTest {
+
+
+    @Test
+    public void initAccountWithCorrectBalanceValue() {
+          }
+
+    @Test
+    public void withdrawWithCorrectBalanceValue() {
+        }
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void depositInvalidAmountAndGetException() {
+        }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void depositNullValueAndGetException() {
+     }
+
+    @Test
+    public void depositTheCorrectBalanceValue() {
+      }
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void withdrawInvalidAmountAndGetException() {
+        //When
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void nullWithrawWillThrowException() {
+        //When
+    }
+
+
+    @Test
+    public void printHistoryOfOperations() {
+
+    }
+}
